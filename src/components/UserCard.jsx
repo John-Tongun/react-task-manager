edd8f2ed1fc5325ef1e7aaf96c845fa01a1e633d
John Doe <jtongun14@gmail.com>
2026-06-10 16:02:50 +0200

    app components updated

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,8 @@
+export default function UserCard({user}) {
+    return (
+        <div className="user-card">
+            <h3>{John Tongun wani}</h3>
+            <p>Fullstack software developer</p>
+        </div>
+    );
+}
---
+++ src/components/siderbar.jsx
@@ -0,0 +1,27 @@
+import { useLayoutEffect } from "react";
+import {link} from "react-router-dom";
+
+export default function Sidebar() {
+    return (
+        <aside>
+            <ul>
+                <li>
+                    <Link to="/">dashboard</Link>
+                    <li>
+                    <Link to="/tasks">tasks</Link>
+                    </li>
+                    <li>
+                    <Link to="/reports">Reports</Link>
+                    </li>
+                </li>
+                    <li>
+                    <Link to="/profile">Profile</Link>
+                    </li>
+                    <li>
+                    <Link to="/settings">Settings</Link>
+                    </li>
+
+            </ul>
+        </aside>
+    );
+}
